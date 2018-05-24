2619ef43fa948ea97645fab9134585d91ff86945
DESKTOP-LDB44O7\dell <15914380151@139.com>
2018-05-24 14:05:28 +0800

    test

--- jiangdaxianService-service/src/main/java/com/jiangdaxian/helloword/service/HellowordService.java
+++ jiangdaxianService-service/src/main/java/com/jiangdaxian/helloword/service/HellowordService.java
@@ -1,10 +1,16 @@
 package com.jiangdaxian.helloword.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jiangdaxian.jdxtest.dao.JdxTestDao;
 
 @Service
 public class HellowordService {
+	@Autowired
+	private JdxTestDao jdxTestDao;
+	
 	public String sayHello() {
-		return "jdx hello world";
+		return "jdx hello world," + jdxTestDao.selectById(1L);
 	}
 }

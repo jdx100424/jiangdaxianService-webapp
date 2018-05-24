589ec30f129f4f8ea37703d5b77a8c93281f5353
DESKTOP-LDB44O7\dell <15914380151@139.com>
2018-05-24 14:09:20 +0800

    logger

--- jiangdaxianService-service/src/main/java/com/jiangdaxian/helloword/service/HellowordService.java
+++ jiangdaxianService-service/src/main/java/com/jiangdaxian/helloword/service/HellowordService.java
@@ -1,16 +1,23 @@
 package com.jiangdaxian.helloword.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.fastjson.JSONObject;
 import com.jiangdaxian.jdxtest.dao.JdxTestDao;
 
 @Service
 public class HellowordService {
+	private static final Logger LOG = LoggerFactory.getLogger(HellowordService.class);
+
 	@Autowired
 	private JdxTestDao jdxTestDao;
 	
 	public String sayHello() {
-		return "jdx hello world," + jdxTestDao.selectById(1L);
+		String str = JSONObject.toJSONString(jdxTestDao.selectById(1L)) ;
+		LOG.info("str:{}",str);
+		return "jdx hello world," +str;
 	}
 }

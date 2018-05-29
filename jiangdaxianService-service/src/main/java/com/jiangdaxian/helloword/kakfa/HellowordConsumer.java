69e2bcee444854a10e520e3f0255805c8d278d69
DESKTOP-LDB44O7\dell <15914380151@139.com>
2018-05-29 17:55:08 +0800

    kafak 改为泛型

--- jiangdaxianService-service/src/main/java/com/jiangdaxian/helloword/kakfa/HellowordConsumer.java
+++ jiangdaxianService-service/src/main/java/com/jiangdaxian/helloword/kakfa/HellowordConsumer.java
@@ -1,14 +1,15 @@
 package com.jiangdaxian.helloword.kakfa;
 
+import java.util.Calendar;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.alibaba.fastjson.JSONObject;
 import com.jiangdaxian.helloword.service.HellowordService;
 import com.jiangdaxian.kafka.BaseConsumer;
-import com.jiangdaxian.kafka.dto.MessageDto;
 
-public class HellowordConsumer extends BaseConsumer {
+public class HellowordConsumer extends BaseConsumer<Calendar> {
 	private static final Logger LOG = LoggerFactory.getLogger(HellowordService.class);
 
 	public HellowordConsumer(String topicName, String groupId,String kafkaIp, String kafkaPort, String kafkaServer) {
@@ -16,7 +17,7 @@ public class HellowordConsumer extends BaseConsumer {
 	}
 
 	@Override
-	public void onMessage(MessageDto dto) {
-		LOG.info("kafka receive:" + JSONObject.toJSONString(dto.getMessageInfo()));
+	public void onMessage(Calendar calendar) {
+		LOG.info("kafka receive:" + JSONObject.toJSONString(calendar.getTime()));
 	}
 }

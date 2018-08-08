acc68334874273e67ec5fa26aae43fd10b105193
jdx100424 <15914380151@139.com>
2018-08-08 16:29:00 +0800

    能区分发布环境的方式

--- jiangdaxianService-webapp/src/test/java/com/jiangdaxian/test/BaseTestCase.java
+++ jiangdaxianService-webapp/src/test/java/com/jiangdaxian/test/BaseTestCase.java
@@ -3,11 +3,10 @@
  */
 package com.jiangdaxian.test;
 
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,6 +20,7 @@ import com.jiangdaxian.base.WebApplication;
  * @Created Date:2016年7月3日
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
 @SpringBootTest(classes=WebApplication.class)// 指定spring-boot的启动类 
 public class BaseTestCase {
 	private static ObjectMapper mapper = new ObjectMapper();
